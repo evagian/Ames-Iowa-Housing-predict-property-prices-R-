487b72b7663ac350cdb185e442a472321b7b3227
Eva Giannatou <eva_giannatou@yahoo.com>
2018-05-22 15:25:09 +0300

    Add files via upload

--- step5-predictive-models-stepwise-regression.R
+++ step5-predictive-models-stepwise-regression.R
@@ -72,4 +72,135 @@ residualPlots(m, plot=F)
 spreadLevelPlot(m)
 
 
+#################################
+#12 -	Plot model results
+#################################
+
+# added variable plots 
+av.plots(m)
+
+# Normality of Residuals
+# qq plot for studentized resid
+qqPlot(m, main="QQ Plot")
+
+# distribution of studentized residuals
+library(MASS)
+sresid <- studres(m) 
+hist(sresid, freq=FALSE, 
+     main="Distribution of Studentized Residuals")
+xfit<-seq(min(sresid),max(sresid),length=40) 
+yfit<-dnorm(xfit) 
+lines(xfit, yfit)
+
+# visualize variables distribution
+library(vioplot)
+
+Mtotal[, 1:20] <- sapply(Mtotal[, 1:20], as.numeric)
+
+Mtotalnums <- Mtotal[, 1:9]
+eda.plots <- function(Mtotalnums, ask=F){
+  graphics.off()
+  numeric.only <- sapply(Mtotalnums,class)=='numeric'
+  y <- Mtotalnums[,numeric.only]
+  n<-ncol(y) 
+  for (i in 1:n){
+    if (!ask) win.graph()
+    par(mfrow=c(2,2), ask=ask)
+    
+    y1 <- y[,i]
+    vioplot(y1)
+    hist(y1, probability=TRUE, main=names(y)[i])
+    lines(density(y1), col=2)
+    qqnorm(y1, main=names(y)[i])
+    qqline(y1)
+    boxplot(y1, main=names(y)[i], horizontal=TRUE)}}
+eda.plots(Mtotalnums)
+
+# plot correlation between attributes
+library(corrplot)
+corrplot((cor(Mtotal)), method="square")
+plotCorr <- function(Mtotal, i){
+  data <- data.frame(x = Mtotal[[i]], SalePrice = Mtotal$SalePrice)
+  p <- ggplot(data, aes(x = x, y = SalePrice)) + geom_point(shape = 1, na.rm = TRUE) + geom_smooth(method = lm ) + xlab(paste0(colnames(Mtotal)[i], '\n', 'R-Squared: ', round(cor(Mtotal[[i]], data$SalePrice, use = 'complete.obs'), 2))) + theme_light()
+  return(suppressWarnings(p))
+}
+doPlots(data_corr, fun = plotCorr, ii = 1:6)
+
+
+#################################
+#13 -	Evaluate regression model assumptions
+#################################
+
+# Evaluate homoscedasticity
+# non-constant error variance test
+lmtest::bptest(m) 
+ncvTest(m)
+
+# Evaluate Collinearity
+vif(m) # variance inflation factors 
+sqrt(vif(m)) > 2 # problem?
+
+#fix collinearity
+library(gvlma)
+gvmodel2 <- gvlma(m) 
+summary(gvmodel2)
+
+# Evaluate Nonlinearity
+# component + residual plot 
+crPlots(m)
+
+# Test for Autocorrelated Errors
+library(lmtest);dwtest(m) # Global test of model assumptions
+
+library(car); durbinWatsonTest(m)
+
+# Global test of model assumptions
+library(gvlma)
+gvmodel <- gvlma(m) 
+summary(gvmodel)
+
+# plot and evaluate residuals
+eruption.res = resid(Mtotal)
+plot(Mtotal$SalePrice, eruption.res, 
+     ylab="Residuals", xlab="Sale Price", 
+     main="Residual Plot") 
+abline(0, 0)
+
+#A density plot
+plot(density(resid(m))) 
+
+# A quantile normal plot - good for checking normality
+qqnorm(resid(m)) 
+qqline(resid(m))
+dev.off()
+residualPlot(m, type='rstudent')
+
+#studentised residuals
+Stud.residuals<-rstudent(m)
+yhat <- fitted(m)
+par(mfrow=c(1,2))
+plot(yhat, Stud.residuals)
+abline(h=c(-2,2), col=2, lty=2)
+plot(yhat, Stud.residuals^2)
+abline(h=4, col=2, lty=2)
+
+#Non-constant Variance Score Test 
+#Variance formula: ~ fitted.values
+library(car)
+ncvTest(m)
+
+#Levene's Test for Homogeneity of Variance (center = median)
+yhat.quantiles<-cut(yhat, breaks=quantile(yhat, probs=seq(0,1,0.25)), dig.lab=6)
+table(yhat.quantiles)
+
+leveneTest(rstudent(m)~yhat.quantiles)
+
+# boxplot
+boxplot(rstudent(m)~yhat.quantiles)
+
+# check independence
+plot(rstudent(m), type='l')
+library(randtests); runs.test(m$res)
+
+
 
